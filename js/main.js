46215286d51315f23e581332f72127b03c431c5d
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-21 15:46:37 +0100

    finish task 1

--- js/main.js
+++ js/main.js
@@ -3,27 +3,32 @@ let team = [
     {
         name: "Wayne",
         surname: "FOunder & CEO",
-        src: ".//img/wayne-barnett-founder-ceo.jpg"
+        src: "./img/wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela",
         surname: "Chief Editor",
+        src: "./img/angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Walter",
         surname: "Office Manager",
+        src: "./img/walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela",
         surname: "Social Media Manager",
+        src: "./img/angela-caroll-chief-editor.jpg"
     },
     {
         name: "Scott",
         surname: "Developer",
+        src: "./img/scott-estrada-developer.jpg"
     },
     {
         name: "Barbara",
         surname: "Graphic Designer",
+        src: "./img/angela-lopez-social-media-manager.jpg"
     }
 ]
 console.log(team)
