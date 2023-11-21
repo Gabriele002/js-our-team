f3804ad147ea320bca3f17edfd6c6b96a5ae47bf
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-21 15:32:17 +0100

    finish task 0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 
+let team = [
+    {
+        name: "Wayne",
+        surname: "FOunder & CEO",
+        src: ".//img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Chief Editor",
+    },
+    {
+        name: "Walter",
+        surname: "Office Manager",
+    },
+    {
+        name: "Angela",
+        surname: "Social Media Manager",
+    },
+    {
+        name: "Scott",
+        surname: "Developer",
+    },
+    {
+        name: "Barbara",
+        surname: "Graphic Designer",
+    }
+]
+console.log(team)
+
+
+
+
+
+   //--------------------FUNZIONI----------------------------------//
